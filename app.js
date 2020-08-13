07955c51742cdad2bd548eaeafe35cea50b1b28a
Aidan Farhi <aidanfarhi@Aidans-MacBook-Air.local>
2020-08-13 15:41:43 -0400

    finished lab. html-tag not working. Error: html is not defined.

--- app.js
+++ app.js
@@ -1,73 +1,27 @@
+
 const express = require("express");
 const morgan = require('morgan')
-const timeAgo = require('node-time-ago')
 const postBank = require('./postBank')
+const postList = require('./views/postList')
+const postDetails = require('./views/postDetails')
 
 const app = express();
 app.use(morgan('dev'))
 app.use(express.static('public'))
 
+app.get('/posts', (req, res) => {
+  const list = postBank.list()
+  res.send(postList(list))
+})
 
 app.get('/posts/:id', (req, res, next) => {
-
   const post = postBank.find(req.params.id)
-
   // this will put out a new error for our error handler down below
   if (!post.id) {
     next(new Error(404))
   } else {
-    const html = `
-    <!DOCTYPE html>
-    <html>
-    <head>
-      <title>Wizard News</title>
-      <link rel="stylesheet" href="/style.css" />
-    </head>
-    <body>
-        <header><img src="/logo.png"/>Wizard News</header>
-          <div class='news-item'>
-            <p>
-              <h2>${post.title}</h2>
-              <small>(by ${post.name})</small>
-            </p>
-            ${post.content}
-          </div>
-    </body>
-  </html>
-  `
-  res.send(html)
+    res.send(postDetails(post))
   }
-})
-
-app.get('/posts', (req, res) => {
-  const list = postBank.list()
-  const html = `
-    <!DOCTYPE html>
-    <html>
-    <head>
-      <title>Wizard News</title>
-      <link rel="stylesheet" href="/style.css" />
-    </head>
-    <body>
-      <div class="news-list">
-        <header><img src="/logo.png"/>Wizard News</header>
-        ${list.map(post => `
-          <div class='news-item'>
-            <p>
-              <span class="news-position">${post.id}. ▲</span>
-              <a href="posts/${post.id}">${post.title}</a>
-              <small>(by ${post.name})</small>
-            </p>
-            <small class="news-info">
-              ${post.upvotes} upvotes | ${post.date}
-            </small>
-          </div>`
-        ).join('')}
-      </div>
-    </body>
-  </html>
-  `
-  res.send(html)
 })
 
 // this is waiting for a new Error object
---
+++ views/postList.js
@@ -0,0 +1,25 @@
+
+const timeAgo = require('node-time-ago')
+//require('html-template-tag')
+module.exports = list => `<!DOCTYPE html>
+ <head>
+   <title>Wizard News</title>
+   <link rel="stylesheet" href="/style.css" />
+ </head>
+ <body>
+   <div class="news-list">
+     <header><img src="/logo.png"/>Wizard News</header>
+     ${list.map(post => `
+       <div class='news-item'>
+         <p>
+           <span class="news-position">${post.id}. ▲</span>${post.title}
+           <small>(by ${post.name})</small>
+         </p>
+         <small class="news-info">
+           ${post.upvotes} upvotes | ${timeAgo(post.date)}
+         </small>
+       </div>`
+     ).join('')}
+   </div>
+ </body>
+</html>`
