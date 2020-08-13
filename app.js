9bb8c5ec79468432266725bfe7b94bc17bfc1cd6
Aidan Farhi <aidanfarhi@Aidans-MacBook-Air.local>
2020-08-13 13:52:39 -0400

    up to bonus section

--- app.js
+++ app.js
@@ -1,10 +1,82 @@
 const express = require("express");
 const app = express();
+const morgan = require('morgan')
+const postBank = require('./postBank')
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(morgan('dev'))
+app.use(express.static('public'))
+
+
+app.get('/posts/:id', (req, res, next) => {
+  const post = postBank.find(req.params.id)
+
+  if (!post.id) {
+    next(new Error(404))
+  } else {
+    const html = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>Wizard News</title>
+      <link rel="stylesheet" href="/style.css" />
+    </head>
+    <body>
+        <header><img src="/logo.png"/>Wizard News</header>
+          <div class='news-item'>
+            <p>
+              <h2>${post.title}</h2>
+              <small>(by ${post.name})</small>
+            </p>
+            ${post.content}
+          </div>
+    </body>
+  </html>
+  `
+  res.send(html)
+  }
+})
+
+app.get('/posts', (req, res) => {
+  const list = postBank.list()
+  const html = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>Wizard News</title>
+      <link rel="stylesheet" href="/style.css" />
+    </head>
+    <body>
+      <div class="news-list">
+        <header><img src="/logo.png"/>Wizard News</header>
+        ${list.map(post => `
+          <div class='news-item'>
+            <p>
+              <span class="news-position">${post.id}. ▲</span>
+              <a href="posts/${post.id}">${post.title}</a>
+              <small>(by ${post.name})</small>
+            </p>
+            <small class="news-info">
+              ${post.upvotes} upvotes | ${post.date}
+            </small>
+          </div>`
+        ).join('')}
+      </div>
+    </body>
+  </html>
+  `
+  res.send(html)
+})
+
+app.use(function (err, req, res, next) {
+  console.error(err.message)
+  if (err.message === 404) {
+    res.status(404).send('404 not found')
+  } else {
+    res.status(500).send('Internal server error')
+  }
+})
 
 const PORT = 1337;
-
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
 });
